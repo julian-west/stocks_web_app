19baa38af8688f0bd44e259ede433497ae608883
julian-west <westjc@btopenworld.com>
2021-11-01 20:23:26 +0000

    move data files

--- const.py
+++ const.py
@@ -1,15 +1,14 @@
 import pandas as pd
 
+START_DATE = "2012-01-01"
+INDEX_DATE = "2020-12-30"
 
-START_DATE = '2012-01-01'
-INDEX_DATE = '2019-12-27'
 
-
-df = pd.read_csv("assets/data/stocks.csv")
-STOCKS = list(df['ticker'].values)
-COMPANY_NAMES = list(df['company name'].values)
-WEIGHTS = list(df['weighting'].values)
+df = pd.read_csv("data/stocks.csv")
+STOCKS = list(df["ticker"].values)
+COMPANY_NAMES = list(df["company name"].values)
+WEIGHTS = list(df["weighting"].values)
 
 STOCK_LIST = []
-for i,stock in enumerate(STOCKS):
-    STOCK_LIST.append({'label':COMPANY_NAMES[i],'value':STOCKS[i]})
+for i, stock in enumerate(STOCKS):
+    STOCK_LIST.append({"label": COMPANY_NAMES[i], "value": STOCKS[i]})
